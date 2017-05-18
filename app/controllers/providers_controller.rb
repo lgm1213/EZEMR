4e42ee3bf26b3fd1fda04031ba13cc95bba2dafb
LGM1213 <lgm1213@gmail.com>
2017-05-17 20:11:48 -0400

    added controller logic for providers, added simple enum to provider model, edited form view and show view

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,16 +1,77 @@
 class ProvidersController < ApplicationController
-  def _form
-  end
-
-  def edit
-  end
-
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
+  
   def index
+    @providers = Provider.all
   end
 
   def new
+    @provider = Provider.new
   end
 
-  def show
+  def create
+    @provider = Provider.new(provider_params)
+    if @provider.save
+      # @user.sent_activation_email
+      flash[:success] = "Welcome to EZ EHR"
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
+
+  def edit
+    @provider = Provider.find(params[:id])
+  end
+
+  def update
+    @provider = Provider.find(param[:id])
+    if @provider.update_attributes(provider_params)
+    #handles successful update of user info
+    flash[:success] = "Updated User Info"
+    redirect_to @provider
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+    @provider = Provider.find(params[:id])
+    redirect_to root_url and return unless true
+  end
+
+  def destory
+    Provider.find(params[:id]).destroy
+    flash[:success] = "User Account Deleted from System"
+    redirect_to users_url
+  end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_provider
+    @provider = Provider.find(params[:id])
+  end
+  
+  def provider_params
+    params.require(:provider).permit(:username, :first_name, :last_name, :middle_initial, :email, :password, :password_confirmation, :gender_cd, :phone, :address, :city, :zip, :state, :npi)
+  end
+
+  #confirm user is logged in
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_provider
+    @provider = Provider.find(params[:id])
+    redirect_to(root_url) unless current_provider?(@provider)
+  end
+
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,2 +1,4 @@
 class Provider < ApplicationRecord
+	#uses simple_enu gemm
+  as_enum :gender, :Female => 1, :Male => 0
 end
